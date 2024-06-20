9f2f27a997ca71e324272dec5e20ea7c60cbd5a2
Ankur Kumar <ankurkumarrr1@gmail.com>
2024-06-21 00:27:57 +0530

    added modifier defined struct Request

--- ethereum/contracts/campaign.sol
+++ ethereum/contracts/campaign.sol
@@ -1,14 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity >=0.7.0 <0.9.0;
 
-contract CampaignFactory {
+contract CampaignContract {
 
-    address public manager;
-    unit public minimumContribution;
-    address[] public approvers;
-
-    function Campaign (uint minimum) public {
-        manager = msg.sender;
-        minimumContribution = minimum;
+    struct Request{
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
     }
 
-}   
+    Request[] public requests;
+    address public manager;
+    uint public minContribution;
+    address[] public approvers;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function Campaign(uint min) public {
+        manager = msg.sender;
+        minContribution = min;
+    }
+
+    function Contribute() public payable {
+        require(msg.value > minContribution);
+
+        approvers.push(msg.sender);
+    } 
+}
