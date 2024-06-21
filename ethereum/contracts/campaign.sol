8a224a55fadd154ea9bee474ee5dd23d2a032c4c
Ankur Kumar <ankurkumarrr1@gmail.com>
2024-06-21 11:41:14 +0530

    array to map for approvers, approve functions added

--- ethereum/contracts/campaign.sol
+++ ethereum/contracts/campaign.sol
@@ -2,20 +2,22 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract CampaignContract {
+contract Campaign {
 
     struct Request{
         string description;
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
 
     Request[] public requests;
     address public manager;
     uint public minContribution;
-    address[] public approvers;
-
+    mapping(address => bool) public approvers;
+    
     modifier restricted() {
         require(msg.sender == manager);
         _;
@@ -29,17 +31,27 @@ contract CampaignContract {
     function Contribute() public payable {
         require(msg.value > minContribution);
 
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
     } 
 
     function createRequests(string memory description, uint value, address recipient) public restricted {
-        Request memory newRequest = Request({
-            description: description,
-            value: value,
-            recipient: recipient,
-            complete: false
-        });
-
-        requests.push(newRequest); 
+        Request storage newRequest = requests.push();
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+        
     }
+
+    function approveRequest(uint index) public  {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+        
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
 }
