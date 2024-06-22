f157c2a77774ede26885f19606cb708ce27f0b3e
Ankur Kumar <ankurkumarrr1@gmail.com>
2024-06-22 16:21:18 +0530

    truffle

--- truffle-config.js
+++ truffle-config.js
@@ -1,25 +1,22 @@
-require('babel-register');
-require('babel-polyfill');
-
 module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
       port: 7545,
-      network_id: '5777'
+      network_id: '*', // Match any network id
     },
   },
-  contracts_directory: './ethereum/contracts',
-  contracts_build_directory: './ethereum/build',
+  contracts_directory: './contracts',
+  contracts_build_directory: './build/contracts',
   compilers: {
     solc: {
       version: '0.8.26',
       settings: {
         optimizer: {
           enabled: true,
-          runs: 200
-        }
-      }
-    }
-  }
+          runs: 200,
+        },
+      },
+    },
+  },
 };
